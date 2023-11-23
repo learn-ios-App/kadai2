26891a459a645c0bc6662847405b49ccacd8bf4d
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-11-23 18:33:52 +0900

    commit

--- kadai2/ContentView.swift
+++ kadai2/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number1 = ""
+    @State private var number2 = ""
+    @State private var bmi = 0
+    
+    @State private var isAlert = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                TextField("", text: $number1)
+                TextField("", text: $number2)
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .frame(width: 200)
+            .padding()
+            Button(action: {
+                bmi = parseNumber(number: number1) + parseNumber(number: number2)
+            }) {
+                Text("計算")
+            }
+            Text("合計 \(bmi)")
+                .padding()
         }
-        .padding()
+    }
+    private func parseNumber(number: String) -> Int {
+        return Int(number) ?? 0
     }
 }
 
